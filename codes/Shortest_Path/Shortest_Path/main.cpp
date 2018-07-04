2b2b461d58074207d4715a2e14e7ea8c5a46b758
LuoBingjun <luobingjun@gmail.com>
2018-07-04 21:44:49 +0800

    更新Map

--- codes/Shortest_Path/Shortest_Path/main.cpp
+++ codes/Shortest_Path/Shortest_Path/main.cpp
@@ -1,26 +1,5 @@
-/**
- * COPYRIGHT NOTICE
- * Copyright (c) 2014, Institute of CG & CAD, Tsinghua University.
- * All Rights Reserved.
- * 
- * @file    main.cpp
- * @brief   Write your own code here.
- * 
- * @version 1.1
- * @author  Jackie Pang
- * @e-mail: 15pengyi@gmail.com
- * @date    2012/10/11
- * @reviser	Gong Chao
- * @date	2014/10/13
- * @e-mail:	gcdofree@gmail.com
- * @reviser	Zhang Mingdong
- * @date	2017/10/10
- * @e-mail:	zhangmd14@mails.tsinghua.edu.cn
- */
-
 #include <iostream>
 #include <vector>
-#include "USSolver.h"
 bool *flag;
 int *A;
 int *B;
@@ -100,17 +79,4 @@ void USSolver::Solve(std::istream &inputStream, std::ostream &outputStream)
 			outputStream << std::endl;
 		}
 	}
-}
-
-
-
-int main(int argc, char *argv[])
-{
-	//可在此更改测试文件
-    std::string fileName = "test101.txt"; 
-    
-    USSolver unofficialSolver(fileName);
-    const std::string unofficialAnswer = unofficialSolver.GetAnswer();
-
-    std::cout << "Your Answer:" << unofficialAnswer << std::endl;
 }
