ec6182f5d35a086e87fa77deff38c6744d7ed55a
LuoBingjun <luobingjun@gmail.com>
2018-07-04 17:36:36 +0800

    更新
    更新联通度标准，更新map.csv，更新最大生成树算法

--- codes/Build_Map/Build_Map.py
+++ codes/Build_Map/Build_Map.py
@@ -5,6 +5,14 @@ data0 = list(csv.reader(file0))
 file1 = open("average.csv", "r")
 data1 = list(csv.reader(file1))
 
+# 生成节点csv
+filepoint = open("point.csv", "w", newline='')
+point = []
+for i in data0:
+    point.append([i[1]])
+writer = csv.writer(filepoint)
+writer.writerows(point)
+
 for i in data1:
     i[2] = float(i[2])
 
@@ -32,29 +40,33 @@ for i in data0:
         m += 1
         b.append(data0.index(j))
         if i[5] == j[5]:
-            c.append(0.9)
+            c.append(0.75)
         else:
-            c.append(0.7)
+            c.append(0.5)
         for k in range(2, 5):
             if [v for v in i[k] if v in j[k]] == []:
                 continue
             else:
                 if i[k] != j[k]:
-                    c[m] += 0.05
+                    c[m] += 0.02
                 else:
-                    c[m] += 0.1
+                    c[m] += 0.05
         for k in range(6, 8):
             if [v for v in i[k] if v in j[k]] == []:
                 continue
             else:
                 if i[k] != j[k]:
-                    c[m] += 0.05
+                    c[m] += 0.02
                 else:
-                    c[m] += 0.1
+                    c[m] += 0.05
     a.append(m + 1)
 
-file3 = open("map.csv", "w")
-writer = csv.writer(file3)
+print(len(a))
+print(len(b))
+
+# 生成正向表csv
+filemap = open("map.csv", "w", newline='')
+writer = csv.writer(filemap)
 writer.writerow(a)
 writer.writerow(b)
 writer.writerow(c)
--- codes/Minimum_Spanning_Tree/Minimum_Spanning_Tree/main.cpp
+++ codes/Minimum_Spanning_Tree/Minimum_Spanning_Tree/main.cpp
@@ -1,31 +1,40 @@
 #include <iostream>
+#include <fstream>
 #include <set>
 #include <utility>
+#include <omp.h>
+#include <assert.h>
+
+using namespace std;
 
 int main()
 {
-	//ÇëÔÚ´ËÌî³ä´úÂë
+	ifstream fin("map.csv");
 	int v, e;
-	std::cin >> v >> e;
+	fin >> v >> e;
+
 	int *p = new int[v + 1];
 	int *q = new int[e];
-	int *r = new int[e];
+	double *r = new double[e];
 	for (int i = 0; i < v + 1; i++)
-		std::cin >> p[i];
+		fin >> p[i];
 	for (int i = 0; i < e; i++)
-		std::cin >> q[i];
+		fin >> q[i];
 	for (int i = 0; i < e; i++)
-		std::cin >> r[i];
+	{
+		fin >> r[i];
+		assert(r[i] <= 1.001);
+	}
 
 	std::set<int> used;
 	used.insert(0);
-	std::pair<int, int> *T = new std::pair<int, int>[v - 1];
-	int min = 0;
+	std::pair<int, int> *T = new pair<int, int>[v - 1];
+	long double max = 0;
 
 	for (int a = 0; a < v - 1; a++)
 	{
-		int mind = INT_MAX;
-		int mine;
+		double maxd = 0;
+		int maxe;
 		for (int i = 0; i < v + 1; i++)
 		{
 			if (used.count(i) == 0)
@@ -34,22 +43,21 @@ int main()
 			{
 				if (used.count(q[j]) == 1)
 					continue;
-				if (r[j] < mind)
+				if (r[j] > maxd)
 				{
 					T[a].first = i;
 					T[a].second = q[j];
-					mind = r[j];
-					mine = j;
+					maxd = r[j];
+					maxe = j;
 				}
 			}
 		}
-		used.insert(q[mine]);
-		min = min + r[mine];
+		used.insert(q[maxe]);
+		max = max + r[maxe];
 	}
-
 	for (int i = 0; i < v - 1; i++)
 	{
-		std::cout << "[" << T[i].first << " -> " << T[i].second << "]";
+		cout << "[" << T[i].first << " -> " << T[i].second << "]";
 	}
-	std::cout << " " << min << std::endl;
+	cout << " " << max << endl;
 }
